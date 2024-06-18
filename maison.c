675d67eea2014d2ab2b53169d72f7fdeec778d7b
ishimwegodardquanta66@gmail.com <ishimwegodardquanta66@gmail.com>
2024-06-18 12:06:41 +0200

    add file

--- maison.c
+++ maison.c
@@ -1,2 +1,93 @@
 #include "maison.h"
 
+Maison* createMaison(const char *adress, double x, double y)
+{
+    char buffer[10000];
+
+    FILE *fileCSV = fopen(adress, "r");
+
+
+    Maison *tabMaison = (Maison *)malloc(1*sizeof(Maison));
+
+   // Maison *maison_tmp;
+
+    int ligne=0;
+    int colonnes=0;
+
+    if(fileCSV!=NULL)
+    {
+        while(fgets(buffer, sizeof(buffer), fileCSV))
+        {
+            colonnes=0;
+            ligne++;
+            
+            if(ligne==1)
+            {
+                continue;
+            }
+
+            char *valeur = strtok(buffer, ",");
+            char *endptr;
+
+            while(valeur)
+            {
+                if(colonnes==0)
+                {
+                    tabMaison[0].xpos = strtod(valeur, &endptr);
+                }
+                if(colonnes==1)
+                {
+                    tabMaison[0].ypos = strtod(valeur, &endptr);
+                }
+                if(colonnes==2)
+                {
+                    tabMaison[0].lat = strtod(valeur, &endptr);
+                }
+                if(colonnes==3)
+                {
+                    tabMaison[0].log = strtod(valeur, &endptr);
+                }
+                if(colonnes==4)
+                {
+                    strcpy(tabMaison[0].adress, valeur);
+                }
+                if(colonnes==5)
+                {
+                    tabMaison[0].numMaison = strtod(valeur, &endptr);
+                }
+                if(colonnes==6)
+                {
+                    tabMaison[0].postcode = strtod(valeur, &endptr);
+                }
+
+                if(colonnes==7)
+                {
+                    strcpy(tabMaison[0].region, valeur);
+                }
+                //printf("%s", valeur);
+                valeur  =strtok(NULL, ","); 
+                colonnes++;
+            }
+
+          break;
+
+
+        }
+    }
+
+    fclose(fileCSV);
+
+    return tabMaison;
+}
+
+int main()
+{
+    double x,y;
+    Maison *mesmaison = createMaison("openaddress-liège.csv",x,y);
+
+    
+    printf("(%f,%f)", mesmaison[0].xpos, mesmaison[0].ypos);
+    
+    
+    return 0;
+}
--- maison.h
+++ maison.h
@@ -3,15 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+ 
 typedef struct 
 {
     double xpos;
     double ypos;
+    double lat;
+    double log;
     char adress[10];
+    int numMaison;
+    int postcode;
+    char region[10];
 }Maison;
 
- // Crée une maison nommée 'maison' et situé à la position  (xpos, ypos) 
+ // Crée une maison nommée 'maison' et situé à la position  (xpos, ypos) à la adress 'adress'
 Maison* createMaison(const char *adress, double x, double y);
 
 
