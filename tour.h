8e9d24d6802dacdea9c2ac737500c3772b2723a4
godardhi <ishimwegodardquanta66@gmail.com>
2024-07-09 17:20:26 +0200

    added two functions

--- tour.h
+++ tour.h
@@ -4,33 +4,39 @@
 #include "maison.h"
 
 
-typedef struct tour
+typedef struct tourpos
 {
     char ad[20];
     double xpos;
     double ypos; 
-    struct tour *newAdrees;
+    struct tourpos *newAdrees;
 
+}TourPos;
+
+typedef struct tour
+{
+    TourPos *queuDeTourpos;
+        
 }Tour;
 
-typedef struct tourpos
-{
-        
-}TourPosition;
 
 
-
-// crée un tour vide
-Tour *createEmptyTour(void);
+// Crée un tour vide
+TourPos *createEmptyTour(void);
 
 // Crée un tour contenant les maison présentes dans le fichier 'filename'
-Tour *createTourFromFile(char *filename);
+TourPos *createTourFromFile(char *filename);
 
-// libère la mémoire de tour et maison
-void freetour(Tour *tour, int freeTown);
+// Libère la mémoire de tour et maison
+void freetour(TourPos *tour, int freeTown);
 
 // Ajouter la ville town à la fin du tour 'tour'
-void addMaisonAtTourEnd(Tour *tour, Maison *maison);
+void addMaisonAtTourEnd(TourPos *tour, Maison *maison);
 
+// Ajouter la maison juste après la position 'pos' dans le tour 'tour'
+void addMaisonAfterTourPosition(TourPos *tour, Tour *pos, Maison *maison);
+
+// Renvoie la première position dans le tour 'tour'
+Tour *getTourStartPosition(TourPos *tour);
 
 #endif
