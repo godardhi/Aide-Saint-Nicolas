904f477d91406587d3c065cf217c68ca6d1dcfcf
godardhi <ishimwegodardquanta66@gmail.com>
2024-07-13 16:55:11 +0200

    "new method"

--- modules/tour.c
+++ modules/tour.c
@@ -25,6 +25,71 @@ Tour *createEmptyTour(void)
 
 Tour *createTourFromFile(char *filename)
 {
-    
+    FILE* file = fopen(filename, "r");
+
+    char buffer[200];
+
+    char *token;
+
+    TourPos *new;
+
+    Tour *newTour;
+
+    int lignedeCSV=1;
+    int colonnesdeCSV=0; 
+
+    if(file!=NULL)
+    {
+
+        while(fgets(buffer, sizeof(buffer), file) && lignedeCSV<=INITIAL_TEST_CAPACITY)
+        {
+            // Allocate memory for 'TourPos', 'maison' and 'Tour'
+            new = (TourPos *)malloc(sizeof(TourPos));
+            new->maison = (Maison *)malloc(sizeof(Maison));
+            newTour = (Tour *)malloc(sizeof(Tour));
+
+        
+            newTour->headOfTour = new;
+
+            new->leftMaison=NULL;
+            new->rightMaison=NULL;
+
+            token = strtok(buffer, ",");
+
+            if(new!=NULL && new->maison!=NULL)
+            {
+                while(token && colonnesdeCSV<5)
+                {
+                    if(colonnesdeCSV==0)
+                    {
+                        new->maison->xpos = strtod(token, NULL);
+                    }
+                    if(colonnesdeCSV==1)
+                    {
+                        new->maison->ypos = strtod(token, NULL);
+                    }
+                    if(colonnesdeCSV==4)
+                    {
+                        strcpy(new->maison->adress, token);
+                    }
+
+                    token =strtok(NULL, ","); 
+                    colonnesdeCSV++;
+                }
+            }else
+            {
+                puts("createTourFromFile : MEMORY ALLOCATION ERROR");
+                exit(1);
+            }
+
+            addMaisonAtTourEnd(newTour, new->maison);
+            lignedeCSV++; 
+            colonnesdeCSV=0;
+        }
+    }else
+    {
+        puts("createTourFromFile : no file to read");
+        exit(1);
+    }
 }
 
